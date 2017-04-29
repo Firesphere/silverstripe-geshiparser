e21d8171f078f6f956ed1e87e476643ac1155073
Simon Erkelens <simonerkelens@silverstripe.com>
2017-04-29 16:08:03 +1200

    A bit upgly cleanup of the HTML generated by TinyMCE. Needs some work.

--- code/parser/GeSHiParser.php
+++ code/parser/GeSHiParser.php
@@ -2,6 +2,9 @@
 
 namespace Firesphere\GeSHiParser;
 
+use SilverStripe\View\Parsers\ShortcodeParser;
+use SilverStripe\View\Parsers\ShortcodeHandler;
+
 /**
  * Class GeSHiParser
  *
@@ -9,14 +12,18 @@ namespace Firesphere\GeSHiParser;
  *
  * @package FireSphere\GeSHiParser
  */
-class GeSHiParser
+class GeSHiParser implements ShortcodeHandler
 {
 
+    public static function get_shortcodes()
+    {
+        return ['code'];
+    }
 
     /**
      * @param string $arguments array with the type
      * @param array $code string of the code to parse
-     * @param \ShortcodeParser $parser Parser root user.
+     * @param ShortcodeParser $parser Parser root user.
      * @param string $shortcode
      * @param array $extra
      *
@@ -28,10 +35,18 @@ class GeSHiParser
             /** Assuming most code is PHP. Feel free to update. Should this be a configurable? */
             $arguments['type'] = 'php';
         }
-        $geshi = new \GeSHi(html_entity_decode(str_replace('<br>', "\n", $code)), $arguments['type']);
+        $code = self::br2nl($code);
+        $code = trim(str_replace("</p><p>", "\n", $code));
+        $geshi = new \GeSHi(html_entity_decode($code), $arguments['type']);
         $geshi->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS);
 
-        return $geshi->parse_code();
+        $result = '<h4 class="parsed-code-title">' . (isset($arguments['title']) ? $arguments['title'] : $arguments['type']) . ':</h4>';
+        $result .= $geshi->parse_code();
+        return $result;
+    }
+
+    public static function br2nl($string){
+        return preg_replace('#<br\s*?/?>#i', "\n", $string);
     }
 
 }
